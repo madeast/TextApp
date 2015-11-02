334440df01377d2d46fd5255583092fe8179ab9a
Easton Madsen <eeagle25@comcast.net>
2015-11-02 09:23:04 -0700

    Added Personal Messages

--- app/src/main/java/edu/ctec/textapp/TextActivity.java
+++ app/src/main/java/edu/ctec/textapp/TextActivity.java
@@ -1,5 +1,6 @@
 package edu.ctec.textapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -13,11 +14,17 @@ import android.app.AlertDialog;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class TextActivity extends AppCompatActivity
 {
     private Button textingButton;
     private EditText nuberText;
     private TextView textingView;
+    private EditText smsEdit;
+    private byte[] hold;
+    private int defaultNumber;
 
 
     @Override
@@ -29,27 +36,37 @@ public class TextActivity extends AppCompatActivity
         textingButton = (Button) findViewById(R.id.textingButton);
         textingView = (TextView) findViewById(R.id.textingView);
         nuberText = (EditText) findViewById(R.id.nuberText);
+        smsEdit = (EditText) findViewById(R.id.smsEdit);
+
+        try
+        {
+            FileInputStream fos = openFileInput("defaultNumber");
+            int worked = fos.read(hold,0,9);
+            fos.close();
+            //defaultNumber = 
+            Toast.makeText(getApplicationContext(), worked, Toast.LENGTH_SHORT);
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(), "First Time opening this App!", Toast.LENGTH_SHORT);
+        }
 
         setupListeners();
-
     }
 
     private void setupListeners()
     {
-        textingButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View currentView)
-            {
-                try
-                {
+        textingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View currentView) {
+                try {
                     String phoneNumber = nuberText.getText().toString();
                     String message = textingView.getText().toString();
-                    SmsManager.getDefault().sendTextMessage(phoneNumber, null, "Would you like to play a game?", null, null);
+                    String Text = smsEdit.getText().toString();
+                    SmsManager.getDefault().sendTextMessage(phoneNumber, null, Text, null, null);
 
                     Toast.makeText(currentView.getContext(), "message was sent", Toast.LENGTH_LONG).show();
-                }
-                catch (Exception currentException)
-                {
+                } catch (Exception currentException) {
                     Toast.makeText(currentView.getContext(), "message was not sent", Toast.LENGTH_LONG).show();
                     Toast.makeText(currentView.getContext(), currentException.getMessage(), Toast.LENGTH_LONG).show();
                 }
@@ -61,6 +78,40 @@ public class TextActivity extends AppCompatActivity
     {
         SmsManager mySMSManager = SmsManager.getDefault();
         mySMSManager.sendTextMessage(phoneNumber, null, messageContent, null, null);
+    }
+
+    private void saveRecentNumber(int number)
+    {
+        if(convertInt(number))
+        {
+            try
+            {
+                FileOutputStream fos = openFileOutput("defaultNumber", Context.MODE_PRIVATE);
+                fos.write(String.valueOf(number).getBytes());
+                fos.close();
+
+            }
+            catch(Exception e)
+            {
+                Toast.makeText(getApplicationContext(), "Number Was not Saved", Toast.LENGTH_LONG).show();            }
+
+        }
+
+    }
+
+    private boolean convertInt(int number)
+    {
+        boolean isString = false;
+        try
+        {
+            String.valueOf(number);
+            isString = true;
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(), "Error number Not Converted to string", Toast.LENGTH_SHORT);
+        }
+        return isString;
     }
 
 }
